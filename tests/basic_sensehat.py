1bc7d90a5dffb1bb8c1b006c1e4bd6d7c7671742
Beaverr <squeaky.beaver4133@protonmail.com>
2025-09-02 17:49:38 -0500

    sensehat test

---
+++ tests/basic_sensehat.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+import asyncio
+
+def main():
+    sense = SenseHat()
+    sense.set_imu_config(True, True, True)
+    
+    while True:
+        print(f"Orientation: {sense.orientation}")
+        print(f"Magnetometer: {sense.compass_raw}")
+        print(f"Gyro {sense.gyro_raw}")
+        print(f"Gyro {sense.accel_raw}")
+
+        asyncio.sleep(.1)
+
+if __name__ == "__main__":
+    main()
