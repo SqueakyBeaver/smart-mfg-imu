99c89bb71fa76e45f0d4b8f1aedc030aa4a81a78
Beaverr <squeaky.beaver4133@protonmail.com>
2025-09-07 10:28:07 -0500

    Add preliminary test for precise looping
    This will be useful to ensure we have time accurate data
    This is the first implementation and at the moment only prints the
    current time. In the future, it will (at the very least, attempt to)
    read from the sensor and will output the time variance of each iteration

---
+++ src/tests/precise_polling_rate.py
@@ -0,0 +1,24 @@
+import time
+from collections.abc import Callable
+
+
+def _next_time(t0: float, change_ns):
+    while True:
+        t0 += change_ns
+        yield t0
+
+
+def precise_loop(func: Callable[..., None], interval_ms: float, *args, **kwargs):
+    timer = _next_time(time.perf_counter_ns(), interval_ms * 1e6)
+    while True:
+        func(*args, **kwargs)
+
+        time.sleep((next(timer) - time.perf_counter_ns()) / 1e9)
+
+
+def main():
+    precise_loop(lambda: print(time.time_ns()), 10)
+
+
+if __name__ == "__main__":
+    main()
