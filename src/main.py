4130f7021f3c97029c14fe6015c1e1acdb97f325
Beaverr <squeaky.beaver4133@protonmail.com>
2025-09-17 10:06:53 -0500

    Create IMU class to wrap adafruit bno08x class

--- src/main.py
+++ src/main.py
@@ -1,21 +1,10 @@
 import time
 from collections.abc import Callable
-from scipy.spatial.transform import Rotation as R
 
-import board
-import busio
-from adafruit_bno08x import (
-    BNO_REPORT_ACCELEROMETER,
-    BNO_REPORT_GYROSCOPE,
-    BNO_REPORT_MAGNETOMETER,
-    BNO_REPORT_ROTATION_VECTOR,
-)
-from adafruit_bno08x.i2c import BNO08X_I2C
-
-from typedefs import IMUData
+from typedefs import IMU
 from utils import output_to_csv
 
-bno: BNO08X_I2C
+bno: IMU
 
 
 def _next_time_step(t0: int, change_ns: int):
@@ -23,22 +12,6 @@ def _next_time_step(t0: int, change_ns: int):
         t0 += change_ns
         yield t0
 
-
-def setup():
-    global bno
-
-    i2c = busio.I2C(board.SCL, board.SDA)
-    # Sometimes, the IMU can have an address
-    # that is different from what the library expects
-    try:
-        bno = BNO08X_I2C(i2c)
-    except:
-        bno = BNO08X_I2C(i2c, address=0x4B)
-
-    bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-    bno.enable_feature(BNO_REPORT_GYROSCOPE)
-    bno.enable_feature(BNO_REPORT_MAGNETOMETER)
-    bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 def basic_reading():
     global bno
@@ -52,34 +25,11 @@ def basic_reading():
         file.write(
             "dev_id,time_ms,accel_x,accel_y,accel_z,"
             + "gyro_x,gyro_y,gyro_z,mag_x,mag_y,mag_z,"
-            + "rot_i,rot_j,_rot_k,rot_real\n"
+            + "yaw,pitch,roll\n"
         )
 
         while True:
-            accel_x, accel_y, accel_z = bno.acceleration
-            gyro_x, gyro_y, gyro_z = bno.gyro
-            mag_x, mag_y, mag_z = bno.magnetic
-            rot_i, rot_j, rot_k, rot_real = qrot = bno.quaternion
-            
-            print(qrot)
-            data = IMUData(
-                "bno085-testing",
-                time.perf_counter_ns() - t0,
-                accel_x,
-                accel_y,
-                accel_z,
-                gyro_x,
-                gyro_y,
-                gyro_z,
-                mag_x,
-                mag_y,
-                mag_z,
-                rot_i,
-                rot_j,
-                rot_k,
-                rot_real,
-            )
-
+            data = bno.read_data(t0)
             output_to_csv(file, data)
 
             time.sleep(max((next(timer) - time.perf_counter_ns()) / 1e9, 0))
@@ -90,7 +40,7 @@ def snapshot():
         file.write(
             "snapshot_label,dev_id,time_ms,accel_x,accel_y,accel_z,"
             + "gyro_x,gyro_y,gyro_z,mag_x,mag_y,mag_z,"
-            + "rot_i,rot_j,_rot_k,rot_real\n"
+            + "yaw,pitch,roll\n"
         )
 
         while True:
@@ -105,31 +55,11 @@ def snapshot():
             print("Taking a snapshot in 1...")
             time.sleep(1)
 
-            accel_x, accel_y, accel_z = bno.acceleration
-            gyro_x, gyro_y, gyro_z = bno.gyro
-            mag_x, mag_y, mag_z = bno.magnetic
-            rot_i, rot_j, rot_k, rot_real = bno.quaternion
-
-            data = IMUData(
-                "bno085-testing",
-                0,
-                accel_x,
-                accel_y,
-                accel_z,
-                gyro_x,
-                gyro_y,
-                gyro_z,
-                mag_x,
-                mag_y,
-                mag_z,
-                rot_i,
-                rot_j,
-                rot_k,
-                rot_real,
-            )
+            data = bno.read_data(0)
+            data.time = 0
 
             save_or_redo = input("r to discard the snapshot. Press s to save it\n")
-            
+
             if save_or_redo.lower() == "r":
                 continue
 
@@ -138,32 +68,21 @@ def snapshot():
 
             output_to_csv(file, data)
 
-def yaw_pitch_roll():
-    global bno
-    
-    while True:
-        rot_i, rot_j, rot_k, rot_real = qrot = bno.quaternion
-
-        # The quaternion should never be all 0's,
-        # but it is as soon as the program starts, for some reason
-        if (qrot) == (0, 0, 0, 0):
-            continue
-
-        rot = R.from_quat([rot_i, rot_j, rot_k, rot_real])
-
-        print(rot.as_euler("xyz", degrees=True))
 
 def main():
-    setup()
     print("""Choose what you want to do:
     1. Basic test 
     2. Snapshot orientations""")
     choice = int(input(""))
 
-    choices: dict[int, Callable[..., None]] = {1: basic_reading, 2: snapshot, 3: yaw_pitch_roll}
+    choices: dict[int, Callable[..., None]] = {
+        1: basic_reading,
+        2: snapshot,
+    }
 
     choices[choice]()
 
 
 if __name__ == "__main__":
+    bno = IMU()
     main()
--- src/typedefs.py
+++ src/typedefs.py
@@ -1,4 +1,17 @@
+import time
 from dataclasses import dataclass
+from functools import cache
+
+import board
+import busio
+from adafruit_bno08x import (
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+)
+from adafruit_bno08x.i2c import BNO08X_I2C
+from scipy.spatial.transform import Rotation as R
 
 
 @dataclass
@@ -20,14 +33,67 @@ class IMUData:
     mag_y: float
     mag_z: float
 
-    quat_i: float
-    quat_j: float
-    quat_k: float
-    quat_real: float
+    yaw: float
+    pitch: float
+    roll: float
 
     def __str__(self):
         return (
             f"{self.dev_id},{self.time},{self.accel_x},{self.accel_y},{self.accel_z},"
             + f"{self.gyro_x},{self.gyro_y},{self.gyro_z},{self.mag_x},{self.mag_y},"
-            + f"{self.mag_z},{self.quat_i},{self.quat_j},{self.quat_k},{self.quat_real}"
+            + f"{self.mag_z},{self.yaw},{self.pitch},{self.roll}"
         )
+
+
+class IMU(BNO08X_I2C):
+    def __init__(self):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        # Sometimes, the IMU can have an address
+        # that is different from what the library expects
+        try:
+            super().__init__(i2c)
+        except:
+            super().__init__(i2c, address=0x4B)
+
+        self.enable_feature(BNO_REPORT_ACCELEROMETER)
+        self.enable_feature(BNO_REPORT_GYROSCOPE)
+        self.enable_feature(BNO_REPORT_MAGNETOMETER)
+        self.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
+    def read_data(self, start_time: int) -> IMUData:
+        """
+        Read accelerometer, gyroscope, magnetometer, and orientation data from the IMU
+
+        start_time: The first value of time.perf_counter_ns before this function is run
+        """
+        accel_x, accel_y, accel_z = self.acceleration
+        gyro_x, gyro_y, gyro_z = self.gyro
+        mag_x, mag_y, mag_z = self.magnetic
+        rot_y, rot_p, rot_r = self._quat_to_ypr(*self.quaternion)
+
+        return IMUData(
+            "bno085-testing",
+            time.perf_counter_ns() - start_time,
+            accel_x,
+            accel_y,
+            accel_z,
+            gyro_x,
+            gyro_y,
+            gyro_z,
+            mag_x,
+            mag_y,
+            mag_z,
+            rot_y,
+            rot_p,
+            rot_r,
+        )
+
+    @cache
+    def _quat_to_ypr(self, qx, qy, qz, qw):
+        """
+        Internal function to convert the quaternion rotation to yaw, pitch, and roll
+        """
+        r = R.from_quat([qx, qy, qz, qw])
+
+        yaw, pitch, roll = r.as_euler("xyz", degrees=True)
+        return (yaw, pitch, roll)
